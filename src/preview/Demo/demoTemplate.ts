99d096446e660d84a9c17980fbe16f654dc773d8
igorpichnenko <igorpichnenko@gmail.com>
2021-04-13 17:55:13 +0600

    #5 fix css

--- src/preview/Demo/demoTemplate.ts
+++ src/preview/Demo/demoTemplate.ts
@@ -2,30 +2,30 @@ const demoTemplate = `
 
 <h4  class="panel__title">Панель управления</h4>
 
-<label class="panel__labels panel__checkboxes">V/H Вид
-<input type="checkbox" class="panel__checkbox panel__inputs js-orientation" name="orientation" /></label>
+<label class="panel__labels">V/H Вид
+<input type="checkbox" class="panel__checkbox panel__inputs js-orientation" /></label>
 
-<label class="panel__labels panel__checkboxes">Показать/Скрыть Tooltip
-<input type="checkbox" class="panel__checkbox panel__inputs js-isLabel" name="label"/></label>
+<label class="panel__labels">Показать/Скрыть Tooltip
+<input type="checkbox" class="panel__checkbox panel__inputs js-isLabel"/></label>
 
-<label class="panel__labels panel__checkboxes">Range/Single
-<input type="checkbox" class="panel__checkbox panel__inputs js-isDouble" name="double"/></label>
+<label class="panel__labels">Range/Single
+<input type="checkbox" class="panel__checkbox panel__inputs js-isDouble"/></label>
 
 <div class="panel__items">
 <label class="panel__labels">Max
-<input type="number" class="panel__inputs js-max" name="max"/></label>
+<input type="number" class="panel__inputs js-max" /></label>
 
 <label class="panel__labels">Min
-<input type="number" class="panel__inputs js-min" name="min"/></label>
+<input type="number" class="panel__inputs js-min"/></label>
 
 <label class="panel__labels">From
-<input type="number" class="panel__inputs js-from" name="from" /></label>
+<input type="number" class="panel__inputs js-from"/></label>
 
 <label class="panel__labels">To
-<input type="number" class="panel__inputs js-to" name="to"/></label>
+<input type="number" class="panel__inputs js-to" /></label>
 
 <label class="panel__labels">Шаг
-<input type="number" class="panel__inputs js-step" name="step"/></label>
+<input type="number" class="panel__inputs js-step"/></label>
 </div>
 `;
 
